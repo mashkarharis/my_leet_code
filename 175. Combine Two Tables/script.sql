e89d9863d6e24941be3255b2571fc5ca16b2794f
mashkarharis <mashkarharis@gmail.com>
2026-01-25 13:48:57 +0530

    added possible accepted answer for lc175

---
+++ 175. Combine Two Tables/script.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    p.firstName as firstName,
+    p.lastName as lastName,
+    a.city as city,
+    a.state as state
+FROM Person p LEFT JOIN Address a ON p.personId = a.personId;
